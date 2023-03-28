15151cca8edd2d9320ec355ff6be15f20bdfaa89
rushit5560 <rushit@omtecweb.com>
2023-03-28 17:58:25 +0530

    Code Init

---
+++ food_back/android/app/src/main/kotlin/com/example/food_back/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.food_back
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ food_back/lib/constants/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color blackColor = Colors.black;
+  static const Color whiteColor = Color(0xffFFF6F6);
+}
