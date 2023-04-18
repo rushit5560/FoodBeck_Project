b8d9a170d56774e09f5e9d732c3084b50addaaf5
rushit5560 <rushit@omtecweb.com>
2023-04-18 15:11:36 +0530

    Create api_url.dart

---
+++ food_back/lib/constance/api_url.dart
@@ -0,0 +1,14 @@
+
+class ApiUrl {
+  static String databaseUrl = "http://food.omdemo.co.in/";
+  static String baseUrl = "$databaseUrl/api/";
+
+
+  static String bannerApi = "$baseUrl/banner/";
+  static String registerApi = "$baseUrl/register";
+  static String cmsApi = "$baseUrl/cms/";
+  static String allProductsApi = "$baseUrl/allproducts/";
+  static String filterByCategoryApi = "$baseUrl/filterbycategory/";
+  static String cartApi = "$baseUrl/cart/";
+  static String updateCartApi = "$baseUrl/updatecart";
+}
